15bb5647a27836651b66291fd36176c32fd07497
FachryAdhitya <fachryadhitya@gmail.com>
2021-12-13 16:18:07 +0700

    change img height

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,34 @@
 
     <div class="row justify-content-center flex-wrap">
         <div class="col-12 col-lg-4 mt-4">
-            <div class="card" >
+            <div class="card h-100" >
+                <img src="https://images.unsplash.com/photo-1598135753163-6167c1a1ad65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80" class="card-img-top h-75" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-lg-4 mt-4">
+            <div class="card h-100" >
+                <img src="https://wallpaperaccess.com/full/1288011.jpg" class="card-img-top h-75" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-lg-4 mt-4">
+            <div class="card h-100" >
+                <img src="https://c4.wallpaperflare.com/wallpaper/702/43/725/jakarta-city-cityscape-wallpaper-preview.jpg" class="card-img-top h-75" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-lg-4 mt-4">
+            <div class="card h-100" >
                 <img src="https://www.pngitem.com/pimgs/m/579-5798505_user-placeholder-svg-hd-png-download.png" class="card-img-top" alt="...">
                 <div class="card-body">
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -28,34 +55,7 @@
         </div>
 
         <div class="col-12 col-lg-4 mt-4">
-            <div class="card" >
-                <img src="https://www.pngitem.com/pimgs/m/579-5798505_user-placeholder-svg-hd-png-download.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-12 col-lg-4 mt-4">
-            <div class="card" >
-                <img src="https://www.pngitem.com/pimgs/m/579-5798505_user-placeholder-svg-hd-png-download.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-12 col-lg-4 mt-4">
-            <div class="card" >
-                <img src="https://www.pngitem.com/pimgs/m/579-5798505_user-placeholder-svg-hd-png-download.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-12 col-lg-4 mt-4">
-            <div class="card" >
+            <div class="card h-100" >
                 <img src="https://www.pngitem.com/pimgs/m/579-5798505_user-placeholder-svg-hd-png-download.png" class="card-img-top" alt="...">
                 <div class="card-body">
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
