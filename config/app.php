7133d374e5b153a7e381b57c241a2024f6121e6e
Uto <daffamadeo@gmail.com>
2022-02-13 22:55:55 +0700

    asdadasd

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://sheltered-everglades-78086.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
